6a5cce933709540ce98163216e8d8e5ce33a26bb
Axel Espinosa <axeldavid45@gmail.com>
2021-01-22 11:00:58 -0600

    test: added list by license plate

--- tests/Feature/VehicleControllerTest.php
+++ tests/Feature/VehicleControllerTest.php
@@ -29,7 +29,39 @@ class VehicleControllerTest extends TestCase
         $response->assertHeader('content-type', 'application/json');
     }
 
-    public function test_store_vehicle_client() {
-        
+    public function test_store_vehicle_client()
+    {
+        $data = [
+            'identity_card' => 'EIMA000822HCSSNXA1',
+            'name' => 'Axel Espinosa',
+            'license_plate' => 'XAS-550-G',
+            'brand' => "ford",
+            'type' => 'pickup'
+        ];
+
+        $response = $this->post('/api/vehicle', $data);
+
+        $response->assertStatus(201);
+        $response->assertHeader('content-type', 'application/json');
+        $response->assertJsonCount(1);
+        $this->assertEquals('Record created', $response->json()['message']);
+        $this->assertDatabaseHas('owners', [
+            'identity_card' => $data['identity_card']
+        ]);
+        $this->assertDatabaseHas('vehicles', [
+            'lice_plate' => $data['license_plate']
+        ]);
+    }
+
+    public function test_list_vehicles_byLicense() {
+        Owner::factory()->create(['identity_card' => '123456']);
+        $vehicle = Vehicle::factory()->create(['owner_id' => '123456', 'brand' => 'ford']);
+        $request = $this->get("/api/vehicle/{$vehicle->lice_plate}");
+
+        $jsonDecoded = $request->json()['data'];
+        $request->assertHeader('content-type', 'application/json');
+        $request->assertOk();
+        $request->assertJsonCount(3, 'data');
+        $this->assertEquals($vehicle->lice_plate, $jsonDecoded['placa']);
     }
 }
